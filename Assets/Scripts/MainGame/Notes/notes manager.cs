9af91571af6532b8667a321155cf24af4b5c0b84
WillowHa <liubensunmen@gmail.com>
2024-09-01 18:35:53 +0900

    ノーツを置く位置などを仮設定
    ノーツを置く位置を空オブジェクトで仮設定.
    スクリプトを途中まで制作.

--- Assets/Scripts/MainGame/Notes/notes manager.cs
+++ Assets/Scripts/MainGame/Notes/notes manager.cs
@@ -6,21 +6,21 @@ using UnityEngine.Rendering;
 
 public class notesmanager : MonoBehaviour
 {
-    public GameObject prefab;
-    public Transform[] spawnPoints;
-    public Canvas canvas;
-    private List<SpawnData> spawnList = new List<SpawnData>();
+    public GameObject prefab;                                   //生成するプレハブ
+    public Transform[] spawnPoints;                             //0~14の座標を保持する配列
+    public Canvas canvas;                                       //プレハブを配置するCanvas
+    private List<SpawnData> spawnList = new List<SpawnData>();　
 
     // Start is called before the first frame update
     void Start()
     {
-        LoadCSV("notes manager");
+        LoadCSV("notesManager");
         StartCoroutine(SpawnPrefabs());
     }
 
     void LoadCSV(string fileName)
     {
-        TextAsset csvFile = Resources.Load<TextAsset>(fileName);
+        TextAsset csvFile = Resources.Load<TextAsset>(fileName);    //ResourcesフォルダからCSVファイルを読み込む
         print(csvFile);
         if (csvFile != null)
         {
@@ -50,8 +50,8 @@ public class notesmanager : MonoBehaviour
         foreach(var spawnData in spawnList)
         {
             yield return new WaitForSeconds(spawnData.spawnTime);
-            GameObject spawnedPrefab = Instantiate(prefab, spawnPoints[spawnData.spawnID]);
-            spawnedPrefab.transform.localPosition = Vector3.zero;
+            GameObject spawnedPrefab = Instantiate(prefab, spawnPoints[spawnData.spawnID]); //プレハブを生成し、Canvas内の指定された位置の子オブジェクトとして配置
+            spawnedPrefab.transform.localPosition = Vector3.zero;                           //親オブジェクトのRectTransformに従うように設定
             spawnedPrefab.transform.localScale = Vector3.one;
 
         }
@@ -66,10 +66,5 @@ public class notesmanager : MonoBehaviour
             spawnTime = time;
             spawnID = id;
         }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
