fde3fa7aec81dbbaba3944370c082a911049f32d
kwbtyuhi <kykykakh5656@gmail.com>
2024-09-05 11:41:33 +0900

    title rule changer

--- Assets/Scripts/Title/Title rule change.cs
+++ Assets/Scripts/Title/Title rule change.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
 
 public class Titlerulechange : MonoBehaviour
 {
     public Sprite[] images;
-    public SpriteRenderer spriteRenderer;
+    public Image image;
     private int roundNum = 0;
     // Start is called before the first frame update
 
@@ -20,21 +23,29 @@ public class Titlerulechange : MonoBehaviour
     {
         if (roundNum >= 0 && roundNum < images.Length)
         {
-            spriteRenderer.sprite = images[roundNum];
+            image.sprite = images[roundNum];
         }
 
     }
 
     public void NextRound()
     {
+        roundNum++;
+        if (roundNum>=images.Length)
+        {
+            roundNum = 0;
+            this.gameObject.SetActive(false);
+        }
+
         UpdateSprite();
     }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
-            NextRound();
-        }
+            {
+                NextRound();
+            }
+        
     }
 }
